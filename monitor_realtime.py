a2b6511e9b8a9244b02046d2008f569bdab435be
LioMartin8 <louisma7x@gmail.com>
2026-04-20 10:52:40 +0700

    update: simulasi-day4

---
+++ monitor_realtime.py
@@ -0,0 +1,43 @@
+import time
+
+import docker
+import mlflow
+import notify2
+
+# set mlflow
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("monitor-realtime_simulasi day4")
+
+# set docker
+client = docker.from_env()
+all_container = client.containers.list(all=True)
+status_sebelumnya = {}
+
+# set notify2
+notify2.init("Aplikasi Python")
+
+while True:
+    for container in all_container:
+        all_container = client.containers.list(all=True)
+        notify = notify2.Notification("MONITOR CONTAINER", "container crash!!")
+        exit_code = container.attrs["State"]["ExitCode"]
+        print("=" * 40)
+        print("Data Container")
+        print("=" * 40)
+        print(f"Name       :{container.name}")
+        print(f"Status     :{container.status}")
+        if container.status in status_sebelumnya:
+            if (
+                status_sebelumnya[container.status] == "running"
+                and container.status == "exited"
+            ):
+                notify.show()
+
+                nama_container = container.name
+                print(f"Exit Code  :{exit_code}")
+
+                with mlflow.start_run():
+                    mlflow.log_param("Nama container: ", nama_container)
+                    mlflow.log_metric("Exit Code: ", exit_code)
+        print("=" * 40)
+    time.sleep(30)
